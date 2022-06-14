4170697a4e8c2f4b260d4b6b55f3593d080533cb
KenNguyen <ngkien299@gmail.com>
2022-06-14 20:53:45 +0700

    Presentation and Container Components

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
 import { DISHES } from "./shared/dish";
 import { Component } from "react";
+import Main from "./components/MainComponent";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-    };
-  }
-
   render() {
-    return <Menu dishes={this.state.dishes} />;
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
   }
 }
 export default App;
