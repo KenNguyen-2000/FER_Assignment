b921238b19ae55dfdfe1b7003997f8d3142dee15
KenNguyen <ngkien299@gmail.com>
2022-06-15 19:53:25 +0700

    fix unused

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
-import Menu from "./components/MenuComponent";
-import { DISHES } from "./shared/dish";
+// import { Navbar, NavbarBrand } from "reactstrap";
+// import Menu from "./components/MenuComponent";
+// import { DISHES } from "./shared/dish";
 import { Component } from "react";
 import Main from "./components/MainComponent";
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React /* , { Component } */ from "react";
 import {
   Card,
   CardImg,
   CardImgOverlay,
-  CardText,
-  CardBody,
+  // CardText,
+  // CardBody,
   CardTitle,
 } from "reactstrap";
 import DishDetail from "./DishdetailComponent";
