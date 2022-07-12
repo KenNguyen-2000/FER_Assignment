35a3145e6c580472ed19cd39c62f52a3ee122d58
KenNguyen <ngkien299@gmail.com>
2022-07-12 15:28:36 +0700

    Lab7

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import {
   Card,
   CardImg,
@@ -7,8 +7,16 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Row,
+  Label,
+  Col,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, Errors, LocalForm } from "react-redux-form";
 
 function RenderDish({ dish }) {
   return (
@@ -44,6 +52,130 @@ function RenderComments({ comments }) {
   );
 }
 
+function CommentForm() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [yourname, setYourname] = useState("");
+  const [comment, setComment] = useState("");
+  const [message, setMessage] = useState("");
+  const [valid, setValid] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleSubmit = (values) => {
+    toggleModal();
+  };
+
+  const validate = (val) => {
+    if (val?.length >= 3 && val?.length <= 15) {
+      setMessage("");
+      setValid(true);
+      return true;
+    } else if (val?.length === 0 || val === undefined) {
+      setMessage("Required");
+      setValid(false);
+      return false;
+    } else if (val?.length < 3) {
+      setMessage("Your name must be greater or equal 3 characters");
+      setValid(false);
+      return false;
+    } else if (val?.length > 15) {
+      setMessage("Your name must be  smaller or equal 15 characters!");
+      setValid(false);
+      return false;
+    }
+  };
+
+  return (
+    <div>
+      <Button type="submit" color="primary" onClick={toggleModal}>
+        Submit Comment
+      </Button>
+      <Modal isOpen={isModalOpen} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Login</ModalHeader>
+        <ModalBody>
+          <LocalForm onSubmit={() => handleSubmit()}>
+            <Row className="form-group">
+              <Label htmlFor="rating" md={12}>
+                Rating
+              </Label>
+              <Col md={12}>
+                <Control.select
+                  type="number"
+                  model=".rating"
+                  id="rating"
+                  name="rating"
+                  className="form-control"
+                  onChange={(e) => setRating(e.target.value)}
+                >
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                  <option value={4}>4</option>
+                  <option value={5}>5</option>
+                </Control.select>
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Label htmlFor="yourname" md={12}>
+                Your Name
+              </Label>
+              <Col md={12}>
+                <Control.text
+                  model=".yourname"
+                  id="yourname"
+                  name="yourname"
+                  placeholder="Your Name"
+                  className="form-control"
+                  onChange={(e) => setYourname(e.target.value)}
+                  validators={{
+                    validate,
+                  }}
+                />
+              </Col>
+              <Col md={12}>
+                <Errors
+                  style={{ color: "red" }}
+                  model=".yourname"
+                  show="touched"
+                  messages={{
+                    validate: message,
+                  }}
+                />
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Label htmlFor="comment" md={12}>
+                Comment
+              </Label>
+              <Col md={12}>
+                <Control.textarea
+                  model=".comment"
+                  id="comment"
+                  name="comment"
+                  placeholder="Comment"
+                  className="form-control"
+                  rows="6"
+                  onChange={(e) => setComment(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Col md={{ size: 10 }}>
+                <Button type="submit" color="primary" disabled={!valid}>
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+          </LocalForm>
+        </ModalBody>
+      </Modal>
+    </div>
+  );
+}
+
 const DishDetail = (props) => {
   if (props.dish) {
     return (
@@ -66,6 +198,7 @@ const DishDetail = (props) => {
           </div>
           <div className="col-12 col-md-5 m-1">
             <RenderComments comments={props.comments} />
+            <CommentForm />
           </div>
         </div>
       </div>
